b4cfcd09c149eb906ebef7fdcf519da5d0e22014
Akixxzz <luckypsilo@iskolarngbayan.edu.ph>
2025-08-05 22:03:05 +0800

    log in page

---
+++ frontend-web/src/components/userIcon.jsx
@@ -0,0 +1,13 @@
+function userIcon() {
+    return (
+      <div className="icon-container">
+        <img
+          src="https://drive.google.com/uc?export=view&id=1sOM35bf-aezqg9XiF2kdCDIYifR1-3wy"
+          alt="User Icon"
+          className="login-icon"
+        />
+      </div>
+    );
+  }
+  
+  export default userIcon;
