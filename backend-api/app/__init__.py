e6f66cbe47e4e3b6db0778a87ef653b3280f4b91
Jenna <148486734+jennarms@users.noreply.github.com>
2025-09-24 00:36:36 +0800

    12 hrs expiration fixed

--- backend-api/app/__init__.py
+++ backend-api/app/__init__.py
@@ -43,7 +43,7 @@ def create_app():
 
     # JWT Config
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'super-secret-key')
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=7) 
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=12)
 
     # Initialize extensions
     mysql.init_app(app)
