bdfac67a80e537fc1dc9afbdce9ab4479c8540e9
Jenna <148486734+jennarms@users.noreply.github.com>
2025-10-10 10:57:29 +0800

    fixed minor autscrolls on landing and logout bug

--- frontend-web/src/components/logout_button.jsx
+++ frontend-web/src/components/logout_button.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./logout_button.css"; // Keep this if necessary, or comment out to debug
 
@@ -18,8 +18,14 @@ export function LogoutButton() {
     // 2. Close modal
     closeConfirm();
 
-    // 3. Redirect to login page
-    navigate("/");
+    // 3. Redirect to login page, and prevent going back to previous page
+    navigate("/", { replace: true });
+
+    // 4. Use window.history.pushState to prevent going back
+    window.history.pushState(null, "", window.location.href);
+    window.onpopstate = () => {
+      window.history.pushState(null, "", window.location.href); // Prevent going back to the previous page
+    };
   };
 
   // Close modal on ESC key
