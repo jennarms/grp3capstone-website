ddec67c0ebfed2780ecaebb4703758b3f1af70ca
Akixxzz <luckypsilo@iskolarngbayan.edu.ph>
2025-10-14 04:07:15 +0800

    vite config v2

--- frontend-web/vite.config.js
+++ frontend-web/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/',
+        target: 'http://localhost:5000/' || 'https://grp3capstone-website-1.onrender.com',
         changeOrigin: true,
       },
     },
